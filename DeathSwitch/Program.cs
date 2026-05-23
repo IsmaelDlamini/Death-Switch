49a981dadd8a2c29a21a8d4a50c1503ecf17ecab
Ismail <iii409475@gmail.com>
2026-05-23 21:24:14 +0200

    testing

--- DeathSwitch/Program.cs
+++ DeathSwitch/Program.cs
@@ -7,7 +7,7 @@ namespace DeathSwitch
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World! Ofentse Ismael Luyanda");
 
             app.Run();
         }
--- DeathSwitch/obj/Debug/net9.0/DeathSwitch.AssemblyInfo.cs
+++ DeathSwitch/obj/Debug/net9.0/DeathSwitch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeathSwitch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc6aa2101a82ed45574fb440073e90e94392e1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f312d5266bdabbdf3b165fb6876a576607e9251")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeathSwitch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeathSwitch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
